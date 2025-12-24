4cfb959d1788dcf247e1084a6db398cfcb86f3cb
SeyedMohammadHasan Jafari <smhj4494@gmail.com>
2025-12-24 20:19:12 +0330

    fix bugs - add feed page

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,186 +1,214 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import User from '../models/user.model.js';
-import Movie from '../models/movie.model.js';
-import Review from '../models/review.model.js';
-import { verifyAdminToken } from '../middlewares/auth.middleware.js';
+import User from "../models/user.model.js";
+import Movie from "../models/movie.model.js";
+import Review from "../models/review.model.js";
+import { verifyAdminToken } from "../middlewares/auth.middleware.js";
 
-router.get('/', verifyAdminToken, async (req, res) => {
-    try {
-        const userId = req.userId;
-        const isLogin = userId ? true : false;
-        const isAdmin = true;
-        const movies = await Movie.find({});
-        res.render('./admin/index', {
-            subject: 'MovieReview - Admin',
-            movies: movies,
-            isLogin: isLogin,
-            isAdmin: isAdmin
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get("/", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
+    const isAdmin = true;
+    const movies = await Movie.find({});
+    res.render("./admin/index", {
+      subject: "MovieReview - Admin",
+      movies: movies,
+      isLogin: isLogin,
+      isAdmin: isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.get('/users', verifyAdminToken, async (req, res) => {
-    try {
-        const userId = req.userId;
-        const isLogin = userId ? true : false;
-        const isAdmin = true;
-        const users = await User.find({ role: 'user' });
-        res.render('./admin/users', {
-            subject: 'MovieReview - ',
-            users: users,
-            isLogin: isLogin,
-            isAdmin: isAdmin
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get("/users", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
+    const isAdmin = true;
+    const users = await User.find({ role: "user" });
+    res.render("./admin/users", {
+      subject: "MovieReview - ",
+      users: users,
+      isLogin: isLogin,
+      isAdmin: isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/users/edit', verifyAdminToken, async (req, res) => {
-    try {
-        const userId = req.userId;
-        const isLogin = userId ? true : false;
-        const isAdmin = true;
+router.post("/users/edit", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
+    const isAdmin = true;
 
-        const user_id = req.body.user_id;
-        const user = await User.findById(user_id);
-        res.render('./admin/user_edit', {
-            subject: 'MovieReview - ',
-            user: user,
-            isLogin: isLogin,
-            isAdmin: isAdmin
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    const user_id = req.body.user_id;
+    const user = await User.findById(user_id);
+    res.render("./admin/user_edit", {
+      subject: "MovieReview - ",
+      user: user,
+      isLogin: isLogin,
+      isAdmin: isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/users/edit/save', verifyAdminToken, async (req, res) => {
-    try {
-        const user_id = req.body.user_id;
-        const user = await User.findById(user_id);
-        await User.findByIdAndUpdate(user_id, { nickname: req.body.nickname });
-        res.redirect('/admin/users')
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.post("/users/edit/save", verifyAdminToken, async (req, res) => {
+  try {
+    const user_id = req.body.user_id;
+    const user = await User.findById(user_id);
+    await User.findByIdAndUpdate(user_id, { nickname: req.body.nickname });
+    res.redirect("/admin/users");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/users/delete', verifyAdminToken, async (req, res) => {
-    try {
-        const user_id = req.body.user_id;
-        await User.findByIdAndDelete(user_id);
-        res.redirect('/admin/users');
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.post("/users/delete", verifyAdminToken, async (req, res) => {
+  try {
+    const user_id = req.body.user_id;
+    await User.findByIdAndDelete(user_id);
+    res.redirect("/admin/users");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.get('/movie/add', verifyAdminToken, async (req, res) => {
-    try {
-        res.render('./admin/add_movie', {
-            subject: 'MovieReview - ',
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.post("/users/delete/multi", verifyAdminToken, async (req, res) => {
+  try {
+    const users_id = req.body.user_id;
+    await User.deleteMany({ $in: user_id });
+    res.redirect("/admin/users");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.get('/movie/:id', verifyAdminToken, async (req, res) => {
-    try {
-        const userId = req.userId;
-        const isLogin = userId ? true : false;
-        const isAdmin = true;
-        const movieId = req.params.id;
-        const movie = await Movie.findById(movieId);
-        const reviews = await Review.find({ movie: movie });
-        const users = [];
-        for (let i = 0; i < reviews.length; i++) {
-            users[i] = await User.findById(reviews[i].user)
-        }
-        res.render('./admin/movie', {
-            subject: 'MovieReview - ',
-            movie: movie,
-            reviews: reviews,
-            users: users,
-            isLogin: isLogin,
-            isAdmin: isAdmin
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get("/movie/add", verifyAdminToken, async (req, res) => {
+  try {
+    res.render("./admin/add_movie", {
+      subject: "MovieReview - ",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.get('/movie/edit/:id', verifyAdminToken, async (req, res) => {
-    try {
-        const movieId = req.params.id;
-        const movie = await Movie.findById(movieId);
-        res.render('./admin/edit_movie', {
-            subject: 'MovieReview - ',
-            movie: movie
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+router.get("/movie/:id", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
+    const isAdmin = true;
+    const movieId = req.params.id;
+    const movie = await Movie.findById(movieId);
+    const reviews = await Review.find({ movie: movie });
+    const users = [];
+    for (let i = 0; i < reviews.length; i++) {
+      users[i] = await User.findById(reviews[i].user);
     }
+    res.render("./admin/movie", {
+      subject: "MovieReview - ",
+      movie: movie,
+      reviews: reviews,
+      users: users,
+      isLogin: isLogin,
+      isAdmin: isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/movie/edit/save', verifyAdminToken, async (req, res) => {
-    try {
-        const { movieId, name, description, image, genre, release_year } = req.body;
-        await Movie.findByIdAndUpdate(movieId, { name, description, image, genre, release_year });
-        res.redirect('/admin/movie/' + movieId);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get("/movie/edit/:id", verifyAdminToken, async (req, res) => {
+  try {
+    const movieId = req.params.id;
+    const movie = await Movie.findById(movieId);
+    res.render("./admin/edit_movie", {
+      subject: "MovieReview - ",
+      movie: movie,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/movie/delete', verifyAdminToken, async (req, res) => {
-    try {
-        const movieId = req.body.movieId;
-        await Movie.findByIdAndDelete(movieId);
-        res.redirect('/admin/');
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.post("/movie/edit/save", verifyAdminToken, async (req, res) => {
+  try {
+    const { movieId, name, description, image, genre, release_year } = req.body;
+    await Movie.findByIdAndUpdate(movieId, {
+      name,
+      description,
+      image,
+      genre,
+      release_year,
+    });
+    res.redirect("/admin/movie/" + movieId);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.get('/reviews', verifyAdminToken, async (req, res) => {
-    try {
-        const userId = req.userId;
-        const isLogin = userId ? true : false;
-        const isAdmin = true;
-        const user = await User.findById(userId);
-        // const movies = await Movie.find({});
-        const reviews = await Review.find({});
-        res.render('./admin/reviews', {
-            subject: 'MovieReview - HomePage',
-            reviews: reviews,
-            isLogin: isLogin,
-            isAdmin: isAdmin,
-            user: user
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-
+router.post("/movie/delete", verifyAdminToken, async (req, res) => {
+  try {
+    const movieId = req.body.movieId;
+    await Movie.findByIdAndDelete(movieId);
+    res.redirect("/admin/");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/review/update', verifyAdminToken, async (req, res) => {
-    try {
-        const userId = req.userId;
-        const isLogin = userId ? true : false;
+router.get("/reviews", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
+    const isAdmin = true;
+    const user = await User.findById(userId);
+    // const movies = await Movie.find({});
+    const reviews = await Review.find({}).populate("user");
+    res.render("./admin/reviews", {
+      subject: "MovieReview - HomePage",
+      reviews: reviews,
+      isLogin: isLogin,
+      isAdmin: isAdmin,
+      user: user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-        const reviewId = req.body.reviewId;
-        const status = req.body.status;
-        await Review.findByIdAndUpdate(reviewId, { status: status });
-        res.redirect('/admin/reviews');
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.post("/review/update/approved", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
 
+    const reviewId = req.body.reviewId;
+    const status = "approved";
+    await Review.findByIdAndUpdate(reviewId, { status: status });
+    res.redirect("/admin/reviews");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/review/update/declined", verifyAdminToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isLogin = userId ? true : false;
+
+    const reviewId = req.body.reviewId;
+    const status = "declined";
+    await Review.findByIdAndUpdate(reviewId, { status: status });
+    res.redirect("/admin/reviews");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;
--- routes/pages.route.js
+++ routes/pages.route.js
@@ -3,6 +3,7 @@ const router = Router();
 import User from "../models/user.model.js";
 import Movie from "../models/movie.model.js";
 import Review from "../models/review.model.js";
+import Friendship from "../models/friendship.model.js";
 import Jsonwebtoken from "jsonwebtoken";
 import JWT_SECRET from "../app.js";
 import verifyToken from "../middlewares/auth.middleware.js";
@@ -121,6 +122,58 @@ router.get("/profile/:id", verifyToken, async (req, res) => {
       user: to,
       isLogin: isLogin,
       isFriend: isFriend,
+      isAdmin: isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/feed", verifyToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    const isLogin = user ? true : false;
+    const isAdmin = user.role == "admin" ? true : false;
+    const friendships = await Friendship.find({ from: user });
+    var friends = [];
+    friendships.forEach((frienship) => {
+      friends.push(frienship.to);
+    });
+    var reviews = await Review.find({ user: { $in: friends } })
+      .sort({
+        createdAt: -1,
+      })
+      .populate("user");
+    res.render("feed", {
+      subject: "MovieReview - ",
+      reviews: reviews,
+      user: user,
+      isLogin: isLogin,
+      isAdmin: isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/friends", verifyToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    const isLogin = user ? true : false;
+    const isAdmin = user.role == "admin" ? true : false;
+    const friendships = await Friendship.find({ from: user });
+    var friends = [];
+    friendships.forEach((frienship) => {
+      friends.push(frienship.to);
+    });
+    var friends_user = await User.find({ _id: { $in: friends } });
+    res.render("friends", {
+      subject: "MovieReview - ",
+      user: user,
+      isLogin: isLogin,
+      friends: friends_user,
       isAdmin: isAdmin,
     });
   } catch (error) {
